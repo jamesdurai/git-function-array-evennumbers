2a235b31547f4b73df78023db72048bdef30ff55
jamesdurai <jamesdurai@outlook.com>
2023-06-01 10:18:41 +0530

    Array Even Numbers

---
+++ main.js
@@ -0,0 +1,13 @@
+let arr =[1,2,3,4,5,6,7,8,9,10]
+
+function countEvenNumbers(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  document.write (countEvenNumbers(arr));
